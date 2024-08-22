50cb8af08f871aa69e375a812837d3a8b0f6bdf3
Hiro <skinner.daniel2004@gmail.com>
2024-08-22 12:00:03 -0600

    Upload Section 22

--- Labs/Chapter1/Section22/Course.java
+++ Labs/Chapter1/Section22/Course.java
@@ -3,58 +3,66 @@ package Labs.Chapter1.Section22;
 import java.util.ArrayList;
 
 public class Course {
-	private ArrayList<Student> roster; // Collection of Student objects
+    private ArrayList<Student> roster; // Collection of Student objects
 
-	public Course() {
-		roster = new ArrayList<Student>();
-	}
+    public Course() {
+        roster = new ArrayList<Student>();
+    }
 
-	// Drops a student from course by removing student from course roster 
-	public void dropStudent(String last) {
-		/* Type your code here */
-	}
+    // Drops a student from course by removing student from course roster
+    public void dropStudent(String last) {
+        ArrayList<Student> studentArr = new ArrayList<Student>();
 
-   public void addStudent(Student s) {
-      roster.add(s);
-   }
- 
-   public int countStudents() {
-      return roster.size();
-   }
-   
-   // main
-   public static void main(String args[]) {
-      Course course = new Course();
-      String first;  // first name
-      String last;   // last name
-      double gpa;    // grade point average
-      int beforeCount;
-      
-      first = "Henry";
-      last = "Nguyen";
-      gpa = 3.5;
-      course.addStudent(new Student(first, last, gpa));  // Add 1st student
-      
-      first = "Brenda";
-      last = "Stern";
-      gpa = 2.0;
-      course.addStudent(new Student(first, last, gpa));  // Add 2nd student
-      
-      first = "Lynda";
-      last = "Robison";
-      gpa = 3.2;
-      course.addStudent(new Student(first, last, gpa));  // Add 3rd student
-      
-      first = "Sonya";
-      last = "King";
-      gpa = 3.9;
-      course.addStudent(new Student(first, last, gpa));  // Add 4th student
-      
-      beforeCount = course.countStudents();  // Number of students before dropping any students
-      last = "Stern";
-      course.dropStudent(last); // Should drop Brenda Stern
-      
-      System.out.println("Course size: " + beforeCount + " students");     // Should output 4
-		System.out.println("Course size after drop: " + course.countStudents() + " students");     // Should output 3
-   }    
+        for(Student student: roster){
+            if(student.getLast() != last){
+                studentArr.add(student);
+            }
+        }
+
+        roster = studentArr;
+    }
+
+    public void addStudent(Student s) {
+        roster.add(s);
+    }
+
+    public int countStudents() {
+        return roster.size();
+    }
+
+    // main
+    public static void main(String args[]) {
+        Course course = new Course();
+        String first; // first name
+        String last; // last name
+        double gpa; // grade point average
+        int beforeCount;
+
+        first = "Henry";
+        last = "Nguyen";
+        gpa = 3.5;
+        course.addStudent(new Student(first, last, gpa)); // Add 1st student
+
+        first = "Brenda";
+        last = "Stern";
+        gpa = 2.0;
+        course.addStudent(new Student(first, last, gpa)); // Add 2nd student
+
+        first = "Lynda";
+        last = "Robison";
+        gpa = 3.2;
+        course.addStudent(new Student(first, last, gpa)); // Add 3rd student
+
+        first = "Sonya";
+        last = "King";
+        gpa = 3.9;
+        course.addStudent(new Student(first, last, gpa)); // Add 4th student
+
+        beforeCount = course.countStudents(); // Number of students before dropping any students
+        last = "Stern";
+        course.dropStudent(last); // Should drop Brenda Stern
+
+        System.out.println("Course size: " + beforeCount + " students"); // Should output 4
+        System.out.println("Course size after drop: " + course.countStudents() + " students"); // Should output 3
+    }
 }
